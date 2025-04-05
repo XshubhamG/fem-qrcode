cf83cc8f2e8ff9bc14e4a14023f1ad9110dc9c92
Shubham Giri <shubhammgiri3@gmail.com>
2025-04-05 16:07:56 +0530

    fixed some css

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ document.querySelector("#app").innerHTML = `
     <main class="card">
       <img src="${qrCode}" alt="QR code" class="img" />
       <div class="content">
-        <h2 class="heading">Improve your front-end skills by building projects</h2>
+        <h1 class="heading">Improve your front-end skills by building projects</h1>
         <p class="text">
           Scan the QR code to visit Frontend Mentor and take your coding skills to
           the next level
