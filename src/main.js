dac19798e7bd908974a1b9361834a3fccfa5e34a
Shubham Giri <shubhammgiri3@gmail.com>
2025-04-05 15:58:10 +0530

    finished first fem project

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "./style.css";
+import qrCode from "/images/image-qr-code.png";
+
+document.querySelector("#app").innerHTML = `
+    <main class="card">
+      <img src="${qrCode}" alt="QR code" class="img" />
+      <div class="content">
+        <h2 class="heading">Improve your front-end skills by building projects</h2>
+        <p class="text">
+          Scan the QR code to visit Frontend Mentor and take your coding skills to
+          the next level
+        </p>
+      </div>
+    </main>
+`;
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/fem-qrcode/", // <-- Replace <YOUR_REPOSITORY_NAME>
+});
